2e1adbb1724a6cbb1d90f50b74656da71f97a6be
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-02-02 19:29:26 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email){
+    return fetch(url, postObject)
+    .then(res => res.json())
+    .then(data => {
+        const id = data.id
+        const element = document.createElement('div')
+        element.textContent = `New object with id: ${id}`
+        document.body.append(element)
+    })
+    .catch(error => {
+       const errorMessage = error.message
+       
+        const errorAlert = document.createElement('p')
+        errorAlert.textContent = errorMessage
+        document.body.append(errorAlert)
+    })
+}
+
+
+
+const url = 'http://localhost:3000/users'
+const bodyData = {
+    name: "Steve",
+    email : "steve@steve.com",
+}
+const postObject = {
+    method: "POST",
+    headers:{
+        "Content-Type" : "application/json",
+        "Accept" : "application/json",
+    },
+    body: JSON.stringify(bodyData)
+}
